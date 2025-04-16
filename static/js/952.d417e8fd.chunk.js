0af9aca4e6b7926f200e10bf16b4a52cdbf83da6
Maxime-Gangneux <gangneuxmaxime@gmail.com>
2025-04-16 15:21:14 +0200

    Updates

---
+++ static/js/952.d417e8fd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknom_du_projet=self.webpackChunknom_du_projet||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.d417e8fd.chunk.js.map
